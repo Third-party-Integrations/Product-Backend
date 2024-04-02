3508a0806829f4d07e9a78bda9427cb5a59028da
kshirsagarRam <rkshirsagar@assimilatetechnologies.com>
2024-04-02 14:21:16 +0530

    add-process.env

--- src/models/index.ts
+++ src/models/index.ts
@@ -5,7 +5,7 @@ mongoose.Promise = global.Promise;
 
 const connectToMongoDB = async () => {
     try {
-        await mongoose.connect(`mongodb+srv://kshirsagarram253:BE2T4bFbps9Gp0W6@cluster0.le0uzhj.mongodb.net/`);
+        await mongoose.connect(process.env.DATABASE_URL); 
         console.log("Successfully connected to MongoDB.");
     } catch (error) {
         console.error("Connection error", error);
