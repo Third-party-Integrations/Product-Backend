6348b46d85ada67c04272959e2e22ff19c4135a9
kshirsagarRam <rkshirsagar@assimilatetechnologies.com>
2024-04-02 14:21:22 +0530

    done

--- src/models/index.ts
+++ src/models/index.ts
@@ -5,7 +5,7 @@ mongoose.Promise = global.Promise;
 
 const connectToMongoDB = async () => {
     try {
-        await mongoose.connect(process.env.DATABASE_URL); 
+        await mongoose.connect(process.env.DATABASE_URL); //replace db url
         console.log("Successfully connected to MongoDB.");
     } catch (error) {
         console.error("Connection error", error);
